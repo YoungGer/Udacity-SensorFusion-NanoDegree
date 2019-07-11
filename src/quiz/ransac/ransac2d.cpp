2310b4bb52bf4ec3a833d7a0ca2afebf970bedea
yangguangyao <yangguangyao@baidu.com>
2019-07-11 14:46:03 +0800

    my own plane fitting algo

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -109,18 +109,77 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 
 }
 
+
+std::unordered_set<int> RansacPlane(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+  std::unordered_set<int> inliersResult;
+  srand(time(NULL));
+  
+  // TODO: Fill in this function
+  while (maxIterations--) {
+    
+    // Randomly sample subset and fit line
+    std::unordered_set<int> curr_inliers; 
+    while (curr_inliers.size() != 3) {
+      curr_inliers.insert(rand() % cloud->points.size());
+    }
+
+    auto itr = curr_inliers.begin();
+    float x1 = cloud->points[*itr].x;
+    float y1 = cloud->points[*itr].y;
+    float z1 = cloud->points[*itr].z;
+    itr++;
+    float x2 = cloud->points[*itr].x;
+    float y2 = cloud->points[*itr].y;
+    float z2 = cloud->points[*itr].z;
+    itr++;
+    float x3 = cloud->points[*itr].x;
+    float y3 = cloud->points[*itr].y;
+    float z3 = cloud->points[*itr].z;
+
+    // plane params
+    float a = (y2-y1)*(z3-z1) - (z2-z1)*(y3-y1);
+    float b = (z2-z1)*(x3-x1) - (x2-x1)*(z3-z1);
+    float c = (x2-x1)*(y3-y1) - (y2-y1)*(x3-x1);
+    float d = - (a*x1 + b*y1 + c*z1);
+
+    // Measure distance between every point and fitted line
+    for (int i = 0; i < cloud->points.size(); ++i)
+    {
+      if (curr_inliers.count(i) > 0)
+        continue;
+
+      float nx = cloud->points[i].x;
+      float ny = cloud->points[i].y;
+      float nz = cloud->points[i].z;
+      float dist = fabs(a * nx + b * ny + c * nz + d) / sqrt(a * a + b * b + c * c);
+
+      if (dist < distanceTol)
+        curr_inliers.insert(i);
+    }
+
+    if (curr_inliers.size() > inliersResult.size())
+      inliersResult = curr_inliers;
+
+  }
+  
+  return inliersResult;
+
+}
+
 int main ()
 {
 
 	// Create viewer
 	pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
 
-	// Create data
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
-	
+	// Create data 2d for line fit
+	// pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
+	// std::unordered_set<int> inliers = Ransac(cloud, 50, 0.5);
 
-	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 50, 0.5);
+  // Create data 3d for plane fit
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
+  std::unordered_set<int> inliers = RansacPlane(cloud, 50, 0.5);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
