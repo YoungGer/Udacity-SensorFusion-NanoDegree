aa30d5a5cad5e655390ddfca43194b5471a4af18
yangguangyao <yangguangyao@baidu.com>
2019-08-26 19:10:36 +0800

    finish 1d fft

--- radar_detection/radar-target-generation-and-detection.m
+++ radar_detection/radar-target-generation-and-detection.m
@@ -101,15 +101,17 @@ Mix_reshape = reshape(Mix, Nr, Nd);
  % *%TODO* :
 %run the FFT on the beat signal along the range bins dimension (Nr) and
 %normalize.
-
+sig_fft1 = fft(Mix_reshape, Nr);
+sig_fft1 = sig_fft1 ./ Nr;
 
  % *%TODO* :
 % Take the absolute value of FFT output
+sig_fft1 = abs(sig_fft1);
 
  % *%TODO* :
 % Output of FFT is double sided signal, but we are interested in only one side of the spectrum.
 % Hence we throw out half of the samples.
-
+sig_fft1_clip = sig_fft1(1:Nr/2);
 
 %plotting the range
 figure ('Name','Range from First FFT')
@@ -117,8 +119,7 @@ subplot(2,1,1)
 
  % *%TODO* :
  % plot FFT output 
-
- 
+plot(sig_fft1_clip); 
 axis ([0 200 0 1]);
 
 
